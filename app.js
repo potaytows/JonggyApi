7beefe8048b01e7e62838011b68d6d1914950cd7
643021301-9 <nitipoom.pi@kkumail.com>
2024-03-13 02:17:21 +0700

    APIEmail

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tablesRouter = require('./routes/tables');
 var restaurantsRouter = require('./routes/restaurants');
-var menusRouter = require('./routes/menu');
 var app = express();
 
 // view engine setup
@@ -28,14 +27,13 @@ app.use('/users', usersRouter);
 app.use('/tables',tablesRouter);
 app.use('/restaurants',restaurantsRouter);
 app.use('/image',imageRouter);
-app.use('/menus',menusRouter);
 
 
 const uri2 = "mongodb://127.0.0.1:27017/dragdrop";
 const uri = "mongodb+srv://finalProject:EFpeUnSek3qtwsMf@cluster0.xoovbhu.mongodb.net/finalProject?retryWrites=true&w=majority" 
 mongoose.connect(uri)
-.then((result)=> app.listen(8000, () => {
-  console.log('API is running on ports 8000 http://localhost:8000/');
+.then((result)=> app.listen(3000, () => {
+  console.log('API is running on ports 8000 http://localhost:3000/');
 }))
     .catch((err) => console.log(err))
 app.use(function(req, res, next) {
